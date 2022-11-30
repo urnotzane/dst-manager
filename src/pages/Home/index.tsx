fc2c2473efc0277a837ce7a13d44474efbae7c09
kaylezang <kaylezang@tencent.com>
2022-11-30 21:55:40 +0800

    feat: layout ing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
+import { Layout } from 'antd';
 import React from 'react';
 import styles from './index.less';
 
+const { Header, Content } = Layout;
+
 export default function Page() {
   return (
-    <div>
-      <h1 className={styles.title}>Page home</h1>
-    </div>
+    <>
+      <Layout>
+        <Header>11</Header>
+        <Content>22</Content>
+      </Layout>
+    </>
   );
 }
--- src/pages/Login/components/LoginForm/index.tsx
+++ src/pages/Login/components/LoginForm/index.tsx
@@ -1,9 +1,11 @@
 import { Button, Checkbox, Form, Input } from 'antd';
 import React from 'react';
+import { history } from 'umi';
 
 export const LoginForm: React.FC = () => {
   const onFinish = (values: any) => {
     console.log('Success:', values);
+    history.push('/');
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -45,7 +47,10 @@ export const LoginForm: React.FC = () => {
           <Checkbox>记住密码</Checkbox>
         </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 0, span: 24 }} className="text-center">
+        <Form.Item
+          wrapperCol={{ offset: 0, span: 24 }}
+          className="text-center mb-0"
+        >
           <Button type="primary" htmlType="submit" size="large">
             登录
           </Button>
